0ea10856a8b369255131b662889d36c27b42192b
Ali Rajaii <rajaii_a@yahoo.com>
2020-10-21 18:02:10 -0400

    added Nav.js and app.js populated.  Need to add the rest of landing page  and tweak

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import Nav from './components/Nav.js';
+import HomePage from './components/HomePage.js';
+import Register from './components/Register.js';
+import Login from './components/Login.js';
+import ForgotUsernamePw from './components/ForgotUsernamePw.js';
+import ResetPassword from './components/ResetPassword.js';
+import NotVerified from './components/NotVerified.js';
+// import DashBoard from './components/Dashboard/DashBoard.js';
+// import DashHeader from './components/Dashboard/DashHeader.js';
+// import PrivateRoute from './components/PrivateRoute.js';
+// import CardSetupForm from './components/Payments/CardSetupForm.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+class App extends React.Component {
+ 
+  
+ render() {
+   
+   return (
+     <div>
+        
+            <Nav />
+            <Route exact path='/' render={(props) => <HomePage {...props} />} ></Route>
+            <Route exact path='/register' render={(props) => <Register {...props} />}></Route>
+            <Route exact path='/login' render={(props) => <Login {...props} />}></Route>
+            <Route exact path='/forgotusernamepw' render={(props) => <ForgotUsernamePw {...props} />}></Route>
+            <Route exact path='/resetpassword' render={(props) => <ResetPassword {...props} />}></Route>
+            <Route exact path='/notverified' render={(props) => <NotVerified {...props} />}></Route>
+            {/* <PrivateRoute exact path='/dashboard' component={DashBoard} ></PrivateRoute>
+            <PrivateRoute exact path='/cardsetupform' component={CardSetupForm}></PrivateRoute> */}
+            
+      
+      </div>
+   )
+ }
+
+      
+ 
 }
 
 export default App;
