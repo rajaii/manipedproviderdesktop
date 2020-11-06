96f57eb292ec4f5dedfdd2179e67ab0618696112
Ali Rajaii <rajaii_a@yahoo.com>
2020-11-05 19:10:58 -0500

    validation wired in and working on servicesAndPricing

--- src/components/ServicesAndPricing.js
+++ src/components/ServicesAndPricing.js
@@ -1,15 +1,48 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { editProfile } from '../actions/appActions';
+import * as yup from 'yup';
+
+const serviceNPriceSchema = yup.object().shape({
+    service: yup.string().required('Service is required'),
+    price: yup.string().matches(/^\$(\d{1,3}(\,\d{3})*|(\d+))(\.\d{2})?$/,"Please enter a vaild dollar amount with cents and a leading dollar sign").required("Price is required")
+})
 
 function ServicesAndPricing (props) {
     const [ service, setService ] = useState('');
     const [ price, setPrice ] = useState('');
+    const [ validationError, setValidationError ] = useState({inner: []})
 
 
-    function handleSubmit () {
-
+   async function handleSubmit (e) {
+        e.preventDefault()
+        let vBody = {
+            service: service,
+            price: price,
+            validationError: validationError
+        }
+        let body = {
+            service: service,
+            price: price,
+        }
+        serviceNPriceSchema.validate(vBody, {abortEarly: false})
+    .then(async d => {
+        if (d) {
+        const id = localStorage.getItem('uID')
+        const added =  await props.editProfile(id, body);
+        setService('');
+        setPrice('');
+        window.confirm('Thank you for adding this service and pricing, it will now be available on your profile!');
     }
+})
+    .catch(err => {
+        setValidationError(err);
+    })   
+    
+      
+}
+    
+
 
     return (
         <div className='sPWrap'>
@@ -23,6 +56,8 @@ function ServicesAndPricing (props) {
                 placeholder='enter service'
                 />
                 <button onSubmit={handleSubmit}>Add Service</button>
+                {validationError.inner != undefined && validationError.inner.filter(i => i.message === "Service is required").length > 0 ?  <div className="ErrorB">SERVICE IS REQUIRED RE-ENTER AND CLICK SUBMIT</div> : null}
+
                 <label>Add the {`${props.service}`} service's pricing here (must be of the form $100.00 ie dollar sign, dollar value, period, cents): </label>
                 <input 
                 type='text'
@@ -32,6 +67,9 @@ function ServicesAndPricing (props) {
                 placeholder='enter price'
                 />
                 <button onSubmit={handleSubmit}>Add price</button>
+                {validationError.inner != undefined && validationError.inner.filter(i => i.message === "Price is required").length > 0 ?  <div className="ErrorB">PRICE IS REQUIRED RE-ENTER AND CLICK SUBMIT</div> : null}
+                {validationError.inner != undefined && validationError.inner.filter(i => i.message === "Please enter a vaild dollar amount with cents and a leading dollar sign").length > 0 ?  <div className="ErrorB">"PLEASE ENTER A VALID DOLLAR AMOUNT WITH CENTS AND A LEADING DOLLAR SIGN"</div> : null}
+       
             </form>        
         </div>
     )
