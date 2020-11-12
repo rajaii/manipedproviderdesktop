42dd393d33206a2fd08bb5831c2e298b6e20a17f
Ali Rajaii <rajaii_a@yahoo.com>
2020-11-12 17:58:16 -0500

    ServicesAdded lightly styled

--- src/components/AuthApplyComponents/ServicesAdded.js
+++ src/components/AuthApplyComponents/ServicesAdded.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './Register.css'
+
 function ServicesAdded (props) {
     return (
-        <div>
-            <h1>You have added the service:</h1>
-            <p>Service: {props.service}</p>
-            <p>Price: {props.price}</p>
+        <div className='sAWrap'>
+            <h1 className='innerSA'>You have added the service:</h1>
+            <p className='innerSA'>Service: {props.service}</p>
+            <p className='innerSA'>Price: {props.price}</p>
         </div>
     )
 }
