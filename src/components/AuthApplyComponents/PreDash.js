f763130291e6228aefd3f13fff7dc7602632f0dd
Ali Rajaii <rajaii_a@yahoo.com>
2020-11-13 11:01:06 -0500

    photos for portfolio can be added in predash

--- src/components/AuthApplyComponents/PreDash.js
+++ src/components/AuthApplyComponents/PreDash.js
@@ -71,7 +71,7 @@ class PreDash extends React.Component {
     render() {
 
         const userId = localStorage.getItem('uID');
-        const photoWidget = window.cloudinary.createUploadWidget({
+        const photo1Widget = window.cloudinary.createUploadWidget({
             cloudName: 'maniped', 
             uploadPreset: 'manipedProvider_preset',
             cropping: true}, (error, result) => { 
@@ -79,10 +79,122 @@ class PreDash extends React.Component {
                 console.log('Done! Here is the image info: ', result.info); 
                 console.log(result.info.secure_url)
                 /////////////////////////////////////////////////
-                const body = {work_img_url_1: result.info.secure_url}
+                const body = {work_image_url_1: result.info.secure_url}
                 this.props.editProfile(userId, body);
                 //potential error needed to be give to user here if editProfile does not work
-                window.confirm('Thank you for adding your photo.  Please continue with your application');
+                window.confirm('Thank you for adding your portfolio photo.  Please continue with your application');
+              }
+            }
+          )
+
+          const photo2Widget = window.cloudinary.createUploadWidget({
+            cloudName: 'maniped', 
+            uploadPreset: 'manipedProvider_preset',
+            cropping: true}, (error, result) => { 
+              if (!error && result && result.event === "success") { 
+                console.log('Done! Here is the image info: ', result.info); 
+                console.log(result.info.secure_url)
+                /////////////////////////////////////////////////
+                const body = {work_image_url_2: result.info.secure_url}
+                this.props.editProfile(userId, body);
+                //potential error needed to be give to user here if editProfile does not work
+                window.confirm('Thank you for adding your portfolio photo.  Please continue with your application');
+              }
+            }
+          )
+
+          const photo3Widget = window.cloudinary.createUploadWidget({
+            cloudName: 'maniped', 
+            uploadPreset: 'manipedProvider_preset',
+            cropping: true}, (error, result) => { 
+              if (!error && result && result.event === "success") { 
+                console.log('Done! Here is the image info: ', result.info); 
+                console.log(result.info.secure_url)
+                /////////////////////////////////////////////////
+                const body = {work_image_url_3: result.info.secure_url}
+                this.props.editProfile(userId, body);
+                //potential error needed to be give to user here if editProfile does not work
+                window.confirm('Thank you for adding your portfolio photo.  Please continue with your application');
+              }
+            }
+          )
+
+          const photo4Widget = window.cloudinary.createUploadWidget({
+            cloudName: 'maniped', 
+            uploadPreset: 'manipedProvider_preset',
+            cropping: true}, (error, result) => { 
+              if (!error && result && result.event === "success") { 
+                console.log('Done! Here is the image info: ', result.info); 
+                console.log(result.info.secure_url)
+                /////////////////////////////////////////////////
+                const body = {work_image_url_4: result.info.secure_url}
+                this.props.editProfile(userId, body);
+                //potential error needed to be give to user here if editProfile does not work
+                window.confirm('Thank you for adding your portfolio photo.  Please continue with your application');
+              }
+            }
+          )
+
+          const photo5Widget = window.cloudinary.createUploadWidget({
+            cloudName: 'maniped', 
+            uploadPreset: 'manipedProvider_preset',
+            cropping: true}, (error, result) => { 
+              if (!error && result && result.event === "success") { 
+                console.log('Done! Here is the image info: ', result.info); 
+                console.log(result.info.secure_url)
+                /////////////////////////////////////////////////
+                const body = {work_image_url_5: result.info.secure_url}
+                this.props.editProfile(userId, body);
+                //potential error needed to be give to user here if editProfile does not work
+                window.confirm('Thank you for adding your portfolio photo.  Please continue with your application');
+              }
+            }
+          )
+
+          const photo6Widget = window.cloudinary.createUploadWidget({
+            cloudName: 'maniped', 
+            uploadPreset: 'manipedProvider_preset',
+            cropping: true}, (error, result) => { 
+              if (!error && result && result.event === "success") { 
+                console.log('Done! Here is the image info: ', result.info); 
+                console.log(result.info.secure_url)
+                /////////////////////////////////////////////////
+                const body = {work_image_url_6: result.info.secure_url}
+                this.props.editProfile(userId, body);
+                //potential error needed to be give to user here if editProfile does not work
+                window.confirm('Thank you for adding your portfolio photo.  Please continue with your application');
+              }
+            }
+          )
+
+          const photo7Widget = window.cloudinary.createUploadWidget({
+            cloudName: 'maniped', 
+            uploadPreset: 'manipedProvider_preset',
+            cropping: true}, (error, result) => { 
+              if (!error && result && result.event === "success") { 
+                console.log('Done! Here is the image info: ', result.info); 
+                console.log(result.info.secure_url)
+                /////////////////////////////////////////////////
+                const body = {work_image_url_7: result.info.secure_url}
+                this.props.editProfile(userId, body);
+                //potential error needed to be give to user here if editProfile does not work
+                window.confirm('Thank you for adding your portfolio photo.  Please continue with your application');
+              }
+            }
+          )
+
+          const photo8Widget = window.cloudinary.createUploadWidget({
+            cloudName: 'maniped', 
+            uploadPreset: 'manipedProvider_preset',
+            cropping: true}, (error, result) => { 
+              if (!error && result && result.event === "success") { 
+                console.log('Done! Here is the image info: ', result.info); 
+                console.log(result.info.secure_url)
+                /////////////////////////////////////////////////
+                const body = {work_image_url_8: result.info.secure_url}
+                this.props.editProfile(userId, body);
+                //potential error needed to be give to user here if editProfile does not work
+                window.confirm('Thank you for adding your portfolio photo.  Please continue with your application');
               }
             }
           )
@@ -114,7 +226,33 @@ class PreDash extends React.Component {
                     <button name='massage_services' onClick={this.handleAdd}>Add New Massage Service</button>
                 </div>
                 {this.state.massage_services.openForm && <AddServicesAndPricing close={this.closeSNP} stVal='massage_services' service='massage'/>}
-            
+                
+                <div>
+                    <h1>You can upload a portfolio of up to 8 photos of your work:</h1>
+                    <p>Portfolio photo 1:</p>
+                    <button onClick={() => this.showWidget(photo1Widget)} className="cloudinary-button">Upload Photo</button>
+
+                    <p>Portfolio photo 2:</p>
+                    <button onClick={() => this.showWidget(photo2Widget)} className="cloudinary-button">Upload Photo</button>
+
+                    <p>Portfolio photo 3:</p>
+                    <button onClick={() => this.showWidget(photo3Widget)} className="cloudinary-button">Upload Photo</button>
+
+                    <p>Portfolio photo 4:</p>
+                    <button onClick={() => this.showWidget(photo4Widget)} className="cloudinary-button">Upload Photo</button>
+
+                    <p>Portfolio photo 5:</p>
+                    <button onClick={() => this.showWidget(photo5Widget)} className="cloudinary-button">Upload Photo</button>
+
+                    <p>Portfolio photo 6:</p>
+                    <button onClick={() => this.showWidget(photo6Widget)} className="cloudinary-button">Upload Photo</button>
+
+                    <p>Portfolio photo 7:</p>
+                    <button onClick={() => this.showWidget(photo7Widget)} className="cloudinary-button">Upload Photo</button>
+
+                    <p>Portfolio photo 8:</p>
+                    <button onClick={() => this.showWidget(photo8Widget)} className="cloudinary-button">Upload Photo</button>
+                </div>
             
             </div>
             
