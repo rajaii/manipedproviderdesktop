f0536d09b27d476ba0223ff26870f43ff553b965
Ali Rajaii <rajaii_a@yahoo.com>
2021-01-01 12:57:45 -0500

    wired in the conditional rendering of nameImg in Showcase.js; is a bit hacky and not dry but I DO NOT want to change the way the DB is set up right now and this was the only solution I could thing of or now

---
+++ src/components/Dash/NameImg.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { editProfile } from '../../actions/appActions.js';
+
+function NameImg (props) {
+     
+    return (
+        <div>
+            klsajdflkasdfj;lk
+        </div>
+    )
+}
+
+export default connect(null, { editProfile })(NameImg);
